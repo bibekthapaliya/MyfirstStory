c59cab4c499f80cda2f2d691eb84035f7c9677fd
Bibek Thapaliya <itsmebvek@gmail.com>
2022-01-17 12:15:03 +0545

    This is test

---
+++ .storybook/main.js
@@ -0,0 +1,18 @@
+module.exports = {
+  "stories": [
+    "../src/**/*.stories.mdx",
+    "../src/**/*.stories.@(js|jsx|ts|tsx)",
+    '../src/components/**/*.stories.js'
+  ],
+  
+  "addons": [
+    "@storybook/addon-links",
+    "@storybook/addon-essentials",
+    "@storybook/preset-create-react-app",
+    "@storybook/addon-interactions",
+  ],
+  "framework": "@storybook/react",
+  "core": {
+    "builder": "webpack5"
+  }
+}
---
+++ src/components/para.js
@@ -0,0 +1,9 @@
+import React from 'react';
+
+export const para =({backgroundColor})=>{
+
+return(
+    <p  style={backgroundColor && { backgroundColor }}>This is test task</p>
+)
+
+}
